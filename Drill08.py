402df1d649b5cdef949b2fe2a081f1474ce331f7
승범 김 <wewe231223@kpu.ac.kr>
2023-10-04 22:38:03 +0900

    캐릭터와 풀 바닥 객체 생성 및 출력 ( 수업내용 )

--- Drill08.py
+++ Drill08.py
@@ -1,6 +1,46 @@
 from pico2d import *
+import random
 
 # Game object class here
+
+class Grass:
+    def __init__(self):
+        self.Image = load_image('grass.png')
+
+
+    def draw(self):
+        self.Image.draw(400,30)
+
+    def update(self):
+        pass
+
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100,700), 90
+        self.frame= 0
+        self.image = load_image ('run_animation.png')
+    def update(self):
+        self.frame = random.randint(0,7)
+        self.x += 5
+    def draw(self):
+        self.image.clip_draw(self.frame *100, 0, 100, 100, self.x , self.y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def handle_events():
     global running
@@ -11,11 +51,46 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
-open_canvas()
 
 # initialization code
+def Reset_World():
+    global running
+    running = True
+    global grass
+    global team
+    global world
 
+    
+
+    team = [Boy() for i in range(10)]
+    grass = Grass()
+
+def Update_World():
+    grass.update()
+    for boy in team:
+        boy.update()
+    pass
+
+def Render_World():
+    clear_canvas()
+    grass.draw()
+    for boy in team:
+        boy.draw()
+    update_canvas()
+
+
+
+open_canvas()
 # game main loop code
+Reset_World()
+
+while running:
+    handle_events()
+    Update_World()
+    Render_World()
+    delay(0.05)
+
+
 
 # finalization code
 
